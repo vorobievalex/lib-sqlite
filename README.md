3f1d6dcbe3eb384704d0411780b5516c0e30a99e
Alexander Vorobyev <avorobyev@revelsystems.com>
2015-06-17 18:01:26 +0300

    Initial upload of Sqlite project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lib-sqlite
 SQlite library and reserve copy of SQLite sources
+
+# Fat library compilation
+* Build project for simulator
+* Build project for iOS device.
+* Copy .a files from DerivedData/$BUILD_DIR/Build/Products/
+* Rename Release-iphonesimulator and Release-iphoneos to libSqlite_armv7_arm64.a and libSqlite_i386_x86_64.a
+lipo -create libSqlite_armv7_arm64.a libSqlite_i386_x86_64.a -output libSQLite3.a
---
+++ SqliteTests/SqliteTests.m
@@ -0,0 +1,34 @@
+//
+//  SqliteTests.m
+//  SqliteTests
+//
+//  Created by Arvydas Ciupaila on 20/11/13.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SqliteTests : XCTestCase
+
+@end
+
+@implementation SqliteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
